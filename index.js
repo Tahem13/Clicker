2f6cf7030a894e1e3ba7e0a7c02bfeecb9ff02ed
Toby Hemley <tobyhemley11@hotmail.com>
2025-11-11 22:09:42 +0000

    Added shop.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 let i = 0
+let addAmount = 1
 let counter = document.getElementById("counter")
 
 main.onclick = function() {
-    i++;
+    i = i + addAmount;
     counter.innerHTML = 'Clicks: ' + i;
+};
+
+button1.onclick = function() {
+    if (i >= 50) {
+        i = i - 50;
+        addAmount++;
+        counter.innerHTML = 'Clicks: ' + i;
+    } else {
+        alert('Not enough money.');
+    };
 };
